e7bed82e45afb072db96b747b962cfc606272d21
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 21:45:52 -0500

    A program that asks for the user's name has been written

---
+++ Using_input_function.py
@@ -0,0 +1,5 @@
+#We need the user to write his first name
+#We will use the function INPUT ()
+firstName= input("What is your first name? \n")
+#print the name of the user
+print("Hello "+ firstName)
